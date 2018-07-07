06bf53f4e2c44ef7ea076043bc5c1a846ff847c4
Vi Kumar <grokkingStuff@gmail.com>
2018-07-07 05:28:19 +0400

    Deleted unnecessary file contactRemote.sh

---
+++ connectRemote.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+## Description: Connects to remote server and relays local changes made in git repo and opens a shell in remote server.
+<<author_information>>
+<<license_information>>
+
+<<system_library>>
+
+# Main control flow
+#function main() {
+#  <<preamble_library>>
+#}
+
+#main "$@"
